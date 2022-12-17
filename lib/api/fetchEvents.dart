d566153a2caf0e0e51415f072adb9f491a204526
Pranavchiku <goswami.4@iitj.ac.in>
2022-12-17 20:39:19 +0530

    fix api to get only unique values

--- lib/api/fetchEvents.dart
+++ lib/api/fetchEvents.dart
@@ -10,70 +10,76 @@ List<Cards> Speakers = [];
 List<Cards> LiveEvents = [];
 List SliderCards = [];
 
+List<EventDetail> technicalEvents = [];
+List<EventDetail> entrepreneurialEvents = [];
+List<EventDetail> speakers = [];
+List<EventDetail> exhibitions = [];
+List<EventDetail> informals = [];
 
-List <EventDetail> technicalEvents = [];
-List <EventDetail> entrepreneurialEvents = [];
-List <EventDetail> speakers = [];
-List <EventDetail> exhibitions = [];
-List <EventDetail> informals = [];
+void fetchEvents() async {
+  var response = await http.get(Uri.https('apiv.prometeo.in', '/api/events/'));
 
-void fetchEvents() async{
-  var response = await http.get(Uri.https('apiv.prometeo.in','/api/events/'));
-  
-  if(response.statusCode == 200){
+  if (response.statusCode == 200) {
     var jsonResponse = jsonDecode(response.body) as List;
-    
-    for(var event in jsonResponse){
-      String imageLink = "https://apiv.prometeo.in/${event['image'].substring(19)}";
+
+    for (var event in jsonResponse) {
+      String imageLink =
+          "https://apiv.prometeo.in/${event['image'].substring(19)}";
       Cards eventCard = Cards(
-            eventId:event['id'].toString(),
-            title: event['name'],
-            prize: event['prize'] ?? ' ',
-            imageLink: imageLink,
-            description: event['description'] ?? ' ',
-            isSpeaker: (event['type'] == 'talk'),
-            eventType: event['type'],
-            ruleBookLink: event['rulebook_link'] ?? '',
-            unstopLink: event["external_link"]?? ' ' ,
-            eventLocation: event["venue"] ?? '',
-            eventDate: event["date"] ?? '',
-          );
+        eventId: event['id'].toString(),
+        title: event['name'],
+        prize: event['prize'] ?? ' ',
+        imageLink: imageLink,
+        description: event['description'] ?? ' ',
+        isSpeaker: (event['type'] == 'talk'),
+        eventType: event['type'],
+        ruleBookLink: event['rulebook_link'] ?? '',
+        unstopLink: event["external_link"] ?? ' ',
+        eventLocation: event["venue"] ?? '',
+        eventDate: event["date"] ?? '',
+      );
       EventDetail eventDetail = EventDetail(
-        name: event['name'], 
-        image: imageLink, 
-        description: event['description'] ?? '', 
-        prizeMoney: event['prize'] ?? 'NA', 
-        date: event['date'] ?? '', 
-        isSpeaker: event['isSpeaker'] ?? false, 
-        unstopLink: event['external_link'] ?? '', 
-        eventLocation: event['venue'] ?? '', 
-        eventType: event['type'], 
-        rulebookLink: event['rulebook_link'] ?? '');
-      
-      
-      
-      if(event['registration_open'] == true){
+          name: event['name'],
+          image: imageLink,
+          description: event['description'] ?? '',
+          prizeMoney: event['prize'] ?? 'NA',
+          date: event['date'] ?? '',
+          isSpeaker: event['isSpeaker'] ?? false,
+          unstopLink: event['external_link'] ?? '',
+          eventLocation: event['venue'] ?? '',
+          eventType: event['type'],
+          rulebookLink: event['rulebook_link'] ?? '');
+
+      if (event['registration_open'] == true) {
         LiveEvents.add(eventCard);
       }
-      if(event['type'] == 'technical'){
+      if (event['type'] == 'technical') {
         TechnicalCards.add(eventCard);
         technicalEvents.add(eventDetail);
-      }    
-      else if(event['type'] == 'entrepreneurial'){
+      } else if (event['type'] == 'entrepreneurial') {
         EntrepreneurialCards.add(eventCard);
         entrepreneurialEvents.add(eventDetail);
-      }
-      else if(event['type'] == 'talk'){
+      } else if (event['type'] == 'talk') {
         Speakers.add(eventCard);
         speakers.add(eventDetail);
-      }
-      else if(event['type'] == 'exhibition'){
+      } else if (event['type'] == 'exhibition') {
         Exhibition.add(eventCard);
         exhibitions.add(eventDetail);
-      }
-      else if(event['type'] == 'informal'){
+      } else if (event['type'] == 'informal') {
         informals.add(eventDetail);
       }
     }
   }
+
+  // make every list unique
+  TechnicalCards = TechnicalCards.toSet().toList();
+  EntrepreneurialCards = EntrepreneurialCards.toSet().toList();
+  Exhibition = Exhibition.toSet().toList();
+  Speakers = Speakers.toSet().toList();
+  LiveEvents = LiveEvents.toSet().toList();
+  technicalEvents = technicalEvents.toSet().toList();
+  entrepreneurialEvents = entrepreneurialEvents.toSet().toList();
+  speakers = speakers.toSet().toList();
+  exhibitions = exhibitions.toSet().toList();
+  informals = informals.toSet().toList();
 }
