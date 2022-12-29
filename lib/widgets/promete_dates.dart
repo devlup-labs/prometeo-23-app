7d78e9148083071ab31acb69d65f61c25fefd17e
Pranavchiku <goswami.4@iitj.ac.in>
2022-12-29 11:20:23 +0530

    change to maxLInes in 1 in prometeo_Dates.dart

--- lib/widgets/promete_dates.dart
+++ lib/widgets/promete_dates.dart
@@ -8,74 +8,57 @@ class PrometeoDates extends StatelessWidget {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Container(
-      width: double.maxFinite, 
-      child: Row(  
-        children: [
+        width: double.maxFinite,
+        child: Row(children: [
           Flexible(
             child: Container(
-              height: 66,
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: const Color(0xff179F97).withOpacity(0.5),
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(15), 
-                  bottomLeft: Radius.circular(15))
-              ),
-              child: Center(  
-                child: Row(
+                height: 66,
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    color: const Color(0xff179F97).withOpacity(0.5),
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        bottomLeft: Radius.circular(15))),
+                child: Center(
+                    child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Icon(Icons.location_on, color: Colors.white),
                     SizedBox(width: 5),
                     SizedBox(
-                    width: size.width * 0.28,
-                    child: AutoSizeText(
-                      'IIT Jodhpur',
-                      style: TextStyle(  
-                          color: Colors.white,
-                          fontSize: 18
-                        ),
-                      maxLines: 2,
+                      width: size.width * 0.28,
+                      child: AutoSizeText(
+                        'IIT Jodhpur',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                        maxLines: 1,
                       ),
-                  ),
+                    ),
                   ],
-                )
-              )
-            ),
+                ))),
           ),
           Flexible(
-            child: Container(  
-              height: 66,
-              padding: const EdgeInsets.all(15),
-              decoration:  BoxDecoration( 
-                color: const Color(0xff96FFDF).withOpacity(0.1), 
-                borderRadius: const BorderRadius.only(  
-                  bottomRight: Radius.circular(15),
-                  topRight: Radius.circular(15)
-                )
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,  
-                children: [  
+            child: Container(
+                height: 66,
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                    color: const Color(0xff96FFDF).withOpacity(0.1),
+                    borderRadius: const BorderRadius.only(
+                        bottomRight: Radius.circular(15),
+                        topRight: Radius.circular(15))),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
                   Icon(Icons.calendar_month, color: Colors.white),
                   SizedBox(width: 5),
                   SizedBox(
                     width: size.width * 0.28,
                     child: AutoSizeText(
                       '20th - 22nd Jan',
-                      style: TextStyle(  
-                          color: Colors.white,
-                          fontSize: 18
-                        ),
-                      maxLines: 2,
-                      ),
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                      maxLines: 1,
+                    ),
                   ),
-                ]
-              )
-            ),
+                ])),
           )
-        ]
-      )
-    );
+        ]));
   }
 }
