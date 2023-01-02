55170196ab0f4080b9dc638f6920128df42082f7
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-01-03 00:53:33 +0530

    added a schedule page

---
+++ lib/pages/schedule_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class ScheduleScreen extends StatefulWidget {
+  const ScheduleScreen({super.key});
+
+  @override
+  State<ScheduleScreen> createState() => _ScheduleScreenState();
+}
+
+class _ScheduleScreenState extends State<ScheduleScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(   
+      body: TableCalendar(  
+          firstDay: DateTime.utc(2010, 10, 16),
+          lastDay: DateTime.utc(2030, 3, 14),
+          focusedDay: DateTime.now(),
+      )
+    );
+  }
+}
