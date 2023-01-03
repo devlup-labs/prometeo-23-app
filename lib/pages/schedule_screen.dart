b2676b1d185dae6d669e85bc83235a42484df8b8
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-01-03 17:53:08 +0530

    fixed the starting date

--- lib/pages/schedule_screen.dart
+++ lib/pages/schedule_screen.dart
@@ -16,18 +16,37 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
   Widget build(BuildContext context) {
     return Scaffold(   
       body: SafeArea(
-        child: TableCalendar(  
-            firstDay: DateTime.utc(2010, 10, 16),
-            lastDay: DateTime.utc(2030, 3, 14),
-            focusedDay: DateTime.now(),
-            calendarFormat: CalendarFormat.week,
-            startingDayOfWeek: StartingDayOfWeek.friday,
-            onDaySelected: (selectedDay, focusedDay) => 
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay;
-              }),
-            selectedDayPredicate: (day) => isSameDay(day, _selectedDay),
+        child: Column(
+          children: [
+            TableCalendar(  
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                focusedDay: DateTime.utc(2023, 1, 20),
+                calendarFormat: CalendarFormat.week,
+                startingDayOfWeek: StartingDayOfWeek.friday,
+                headerStyle:const HeaderStyle(   
+                  formatButtonVisible: false,
+                  titleCentered: true,
+                  formatButtonShowsNext: false,
+                ),
+                onDaySelected: (selectedDay, focusedDay) => 
+                  setState(() {
+                    _selectedDay = selectedDay;
+                    _focusedDay = focusedDay;
+                  }),
+                selectedDayPredicate: (day) => isSameDay(day, _selectedDay),
+            ),
+            Expanded(
+              child: ListView.builder( 
+                itemCount: 3,  
+                itemBuilder: (context, builder) => Container(
+                  margin: const EdgeInsets.all(10),
+                  color: Colors.red,
+                  height: 100,
+                ),
+              ),
+            )
+          ],
         ),
       )
     );
