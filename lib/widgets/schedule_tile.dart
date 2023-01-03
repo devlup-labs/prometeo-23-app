51b5339a73d2d84f1d65aa16e050a00714d0d457
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-01-03 21:54:23 +0530

    added a tile for the schedule page

---
+++ lib/widgets/schedule_tile.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
+
+class ScheduleTile extends StatelessWidget {
+  const ScheduleTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GlassmorphicContainer(  
+      margin: const EdgeInsets.all(10),
+      width: double.infinity,
+      height: 100,
+      blur: 10,
+      border: 0,
+      borderRadius: 20,
+      linearGradient: LinearGradient(  
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          const Color(0xFFffffff).withOpacity(0.1),
+          const Color(0xFFFFFFFF).withOpacity(0.05),
+        ],
+        stops: const [0.1, 1],
+      ),
+      borderGradient: LinearGradient(  
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+        colors: [
+          const Color(0xFFffffff).withOpacity(0.5),
+          const Color(0xFFFFFFFF).withOpacity(0.5),
+        ],
+      ),
+
+
+    );
+  }
+}
